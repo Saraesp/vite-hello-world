b23c754da1e827ee9b483e9cab0725cf05a7b570
Saraesp <saraesposito145@gmail.com>
2023-01-10 19:48:18 +0100

    Stampa Titolo

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+export default{
+  name:'AppTitle',
+  data(){
+    return{
+      message:'La mia prima app con Vite!'
+    }
+  }, 
+  methods:{
+
+  }
+}
+</script>
+
+<template lang="">
+    <div>
+        {{message}}
+    </div>
+</template>
+
+<style>
+</style>
