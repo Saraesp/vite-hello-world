8697d6d2f65945c9d9bb499961055feae3d3d9af
Saraesp <saraesposito145@gmail.com>
2023-01-11 13:05:15 +0100

    Stampa immagine

---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script>
+export default{
+  name:'AppHero',
+  data(){
+    return{
+      image: '/immagini-aurora.jpeg'
+    }
+  }, 
+  methods:{
+
+  }
+
+}
+</script>
+
+<template lang="">
+    <div>
+        <img src="./assets/image" alt="">
+    </div>
+</template>
+
+<style>
+  img{
+    width: 200px;
+    height: 200px;
+  }
+</style>
